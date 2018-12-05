49e06e0219ab139e624859d7414df736c1b17f15
Dongwon-Shin <dws1001@cu.ac.kr>
2018-12-05 16:45:47 +0900

    change.py

---
+++ Python/change.py
@@ -0,0 +1,7 @@
+import pandas as pd
+alco2009 = pd.read_csv("niaaa-report2009.csv", index_col="State")
+
+wine_state = alco2009["Wine"] > alco2009["Wine"].mean()
+beer_state = alco2009["Beer"] > alco2009["Beer"].mean()
+pd.crosstab(wine_state, beer_state)
+print(pd.crosstab(wine_state, beer_state))
